9ca0cfb84ae32008ab06851748f5d69fd8441f8d
Andrey Panfilov <corhal@yandex.ru>
2017-08-15 20:21:15 +0300

    init

---
+++ Assets/Scripts/MoveOnClick.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveOnClick : MonoBehaviour {
+
+	bool shouldMove = false;
+
+	public float Speed = 1.0F;
+	float startTime;
+	float journeyLength;
+	Vector2 start;
+	Vector2 target;
+
+	void Update() {	
+		if (Input.GetMouseButtonDown(0)) {
+			shouldMove = true;
+			startTime = Time.time;
+			start = transform.position;
+			target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+			journeyLength = Vector2.Distance(start, target);
+		}
+
+		if (shouldMove) {
+			float distCovered = (Time.time - startTime) * Speed;
+			float fracJourney = distCovered / journeyLength;
+			transform.position = Vector2.Lerp(start, target, fracJourney);
+			if (fracJourney == 1.0f) {
+				shouldMove = false;
+			}
+		}
+
+	}
+}
---
+++ Assets/Scripts/Ship.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour {
+
+	Vector2 shipPosition;
+	Vector2 targetPosition;
+
+	// Use this for initialization
+	void Start () {
+		// 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
