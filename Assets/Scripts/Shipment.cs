d40509683cc56c7d8c52a73cce31a8ef424c2f50
Andrey <corhal@yandex.ru>
2017-08-16 09:54:15 +0300

    reimport and start

---
+++ Assets/Scripts/Shipment.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shipment : MonoBehaviour {
+
+	public string GoodsName;
+	public Location StartLocation;
+	public Location Destination;
+	public string DestinationString;
+	public int Reward;
+
+	void Start() {
+		int distance = Vector2.Distance (StartLocation.transform.position, Destination.transform.position);
+	}
+}
