80025d18350b280f5d7c5fd091914071c5a59334
Andrey <corhal@yandex.ru>
2017-09-12 09:57:49 +0300

    Camera drag; should see if problem was in LateUpdate

---
+++ Assets/Scripts/CameraDrag.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraDrag : MonoBehaviour
+{
+	public float dragSpeed = 2;
+	private Vector3 dragOrigin;
+
+	public bool cameraDragging = true;
+
+	public float outerLeft = -50f;
+	public float outerRight = 50f;
+
+
+	void Update() {		
+		cameraDragging = true;
+
+		/*if (DragController.ShouldDrag || FloorController.isInFloorMode || Restaurant.instance.IsWindowOpen) {
+			cameraDragging = false;
+		}*/
+
+		if (cameraDragging) {
+			if (Input.GetMouseButtonDown(0)) {
+				dragOrigin = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+				//Debug.Log (dragOrigin);
+				return;
+			}
+
+			/*if (!Input.GetMouseButton (0)) {
+				if(this.transform.position.x > outerRight) {
+					transform.position = new Vector3 (outerRight, transform.position.y, transform.position.z);
+				}
+				if(this.transform.position.x < outerLeft) {
+					transform.position = new Vector3 (outerLeft, transform.position.y, transform.position.z);
+				}
+				return;
+			}*/
+
+			if (Input.GetMouseButton(0)) {
+				Vector3 mousePos = Camera.main.ScreenToViewportPoint (Input.mousePosition);
+				Vector3 pos = mousePos - dragOrigin;
+				pos = new Vector3(-pos.x, -pos.y, -10.0f);
+				transform.position = pos;
+			}
+
+			// Camera.main.ScreenToWorldPoint (newPosition);// + offset;
+
+			/*Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
+			Vector3 move = new Vector3(-pos.x * dragSpeed, 0, 0);
+
+			if (move.x > 0f) {
+				if(this.transform.position.x < outerRight) {
+					transform.Translate(move, Space.World);
+				}
+			}
+			else {
+				if(this.transform.position.x > outerLeft) {
+					transform.Translate(move, Space.World);
+				}
+			}*/
+		}
+	}
+}
---
+++ Assets/Scripts/DragController.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DragController : MonoBehaviour {
+
+	GameObject draggable;
+	public bool SnapToGrid;
+	Vector3 offset;
+	bool shouldDrag;
+	public static bool ShouldDrag; // hack
+	public int layerMask;
+
+	void Awake() {
+		//SelectionController.OnSelectionChanged += SelectionController_OnSelectionChanged;
+		layerMask = LayerMask.GetMask ("Buildings");
+	}
+
+	void SelectionController_OnSelectionChanged (Selectable selection, bool selected) {
+		/*if (!selected) {
+			draggable = null;
+			shouldDrag = false;
+		} else if (selection.Moveable) {
+			draggable = selection.gameObject;
+			shouldDrag = true;
+		}
+		ShouldDrag = shouldDrag;*/
+	}
+		
+	void Update() {		
+		if (draggable != null && Input.GetMouseButtonDown(0)) {
+			if (Utility.CastRayToMouse(layerMask) != null && Utility.CastRayToMouse(layerMask).GetComponent<Selectable>() == draggable.GetComponent<Selectable>()) {
+				shouldDrag = true;
+				offset = draggable.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f));
+			} else {
+				shouldDrag = false;
+			}
+		}
+		if (shouldDrag && Input.GetMouseButton(0)) {
+			Debug.Log ("should be dragging right now");
+			Vector3 newPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f);
+			draggable.transform.position = Camera.main.ScreenToWorldPoint(newPosition) + offset;
+			float factor = 0.375f;
+			if (SnapToGrid) {
+				draggable.transform.position = new Vector3 (Utility.SnapNumberToFactor (draggable.transform.position.x, factor), Utility.SnapNumberToFactor (draggable.transform.position.y, factor), Utility.SnapNumberToFactor (draggable.transform.position.z, factor));
+			}
+			/*ZChecker checker = draggable.GetComponent<ZChecker> ();
+			checker.CheckZ ();*/
+		}
+	}
+
+	/*void OnDestroy() {
+		SelectionController.OnSelectionChanged -= SelectionController_OnSelectionChanged;
+	}*/
+}
