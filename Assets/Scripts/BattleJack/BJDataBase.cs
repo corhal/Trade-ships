bfcbfdbb4ebe27576995f3e8038652898d5385ed
Andrey Panfilov <corhal@yandex.ru>
2017-11-01 20:47:58 +0300

    card dealing

--- Assets/Scripts/BattleJack/BJDataBase.cs
+++ Assets/Scripts/BattleJack/BJDataBase.cs
@@ -7,7 +7,7 @@ public enum Suits {
 }
 
 public enum Ranks {
-	Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Ace
+	Two = 2, Three = 3, Four = 4, Five = 5, Six = 6, Seven = 7, Eight = 8, Nine = 9, Ten = 10, Jack = 10, Queen = 10, King = 10, Ace = 11
 }
 
 public class BJDataBase : MonoBehaviour {
--- Assets/Scripts/BattleJack/BJGameController.cs
+++ Assets/Scripts/BattleJack/BJGameController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BJGameController : MonoBehaviour {
 
@@ -10,10 +11,12 @@ public class BJGameController : MonoBehaviour {
 	public GameObject CardObjectPrefab;
 	public List<GameObject> CardObjects;
 
+	int currentScore;
+	public Text ScoreLabel;
 
-	// Use this for initialization
 	void Start () {
 		Deck = new BJDeck ();
+		Deck.Shuffle ();
 		Deal (2);
 	}
 
@@ -22,8 +25,40 @@ public class BJGameController : MonoBehaviour {
 			GameObject cardObject = Instantiate (CardObjectPrefab) as GameObject;
 			BJCardObject bjCardObject = cardObject.GetComponent<BJCardObject> ();
 			bjCardObject.Card = Deck.Deal ();
+			bjCardObject.RankSuitLabel.text = bjCardObject.Card.Rank + "\n" + "of\n" + bjCardObject.Card.Suit;
 			cardObject.transform.SetParent (CardObjectsContainer.transform);
 			cardObject.transform.localScale = Vector3.one;
+			CardObjects.Add (cardObject);
 		}
+		CountScore ();
+	}
+
+	void CountScore () {
+		List<BJCard> Cards = new List<BJCard> ();
+		List<BJCard> Aces = new List<BJCard> ();
+
+		currentScore = 0;
+		foreach (var cardObject in CardObjects) {
+			BJCard card = cardObject.GetComponent<BJCardObject> ().Card; // bad use of GetComponent in foreach
+			if (card.Rank == Ranks.Ace) { 
+				Aces.Add (card);
+			} else {
+				Cards.Add (card);
+			}
+		}
+
+		foreach (var card in Cards) {
+			currentScore += (int)card.Rank;
+		}
+
+		foreach (var ace in Aces) {
+			if (currentScore + (int)ace.Rank > 21) {
+				currentScore += 1;
+			} else {
+				currentScore += (int)ace.Rank;
+			}
+		}
+
+		ScoreLabel.text = "" + currentScore;
 	}
 }
